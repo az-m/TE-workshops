fa85a762a30149fab9cf1fe06b931816178e7d2d
az-m <alisonzmills@gmail.com>
2025-06-02 14:30:49 +0100

    begin wk6

---
+++ wk6/react-workshop/workshop/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <Button />
+    </>
+  );
+}
+
+export default App;
