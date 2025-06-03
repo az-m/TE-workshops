d665a7bbf12da0a3783733a79eef0274f8f9b78d
az-m <alisonzmills@gmail.com>
2025-06-03 10:02:40 +0100

    Monday commit

--- wk6/react-workshop/workshop/src/App.jsx
+++ wk6/react-workshop/workshop/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
 import Button from "./components/Button";
+import ImageFrame from "./components/ImageFrame";
 
 function App() {
   return (
     <>
       <h1>App</h1>
       <Button />
+      <ImageFrame
+        src="https://images.unsplash.com/photo-1589656966895-2f33e7653819"
+        alt="a polar bear waves at the photographer"
+        caption="This polar bear says hello!"
+      />
+      <ImageFrame
+        src="https://images.unsplash.com/photo-1564349683136-77e08dba1ef7"
+        alt="a panda eats bamboo"
+        caption="This panda is too busy to say hello."
+      />
     </>
   );
 }
